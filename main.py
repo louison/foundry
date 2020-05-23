36bad2ea2d301fdffb2f32474f0859d3cc7b1c1e
Rashad Karanouh <karanouhrs@gmail.com>
2020-05-23 17:29:21 +0200

    update docstring main

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ logger.setLevel(logging.DEBUG)
 
 
 def random_tracks(event, context):
-    """Creates a playlist with random tracks from BigQuery
-
-    Arguments:
-        event {dict} -- Some random stuff Google Cloud neeeds
-        context {dict} -- Some other random stuff Google Cloud needs
+    """Get 10 random tracks from BigQuery as a playlist
+    Args:
+        event (dict):  The dictionary with data specific to this type of
+        event. The `data` field contains the PubsubMessage message. The
+        `attributes` field will contain custom attributes if there are any.
+        context (google.cloud.functions.Context): The Cloud Functions event
+        metadata. The `event_id` field contains the Pub/Sub message ID. The
+        `timestamp` field contains the publish time.
     """
     tracks = playlist_random_tracks.get_tracks()
     spotify.push_to_playlist(tracks, "6hlAehDHbcmZXXYCz6sl72")
