216ca6de0faafaf773f3a824ff0ea2d274b4d64d
Louison Gitzinger <gitzingerlouison@gmail.com>
2020-08-02 14:59:54 +0200

    update

--- playlist_maker/auto_playlists/Diggers.py
+++ playlist_maker/auto_playlists/Diggers.py
@@ -36,41 +36,56 @@ class Diggers(AutoPlaylist):
         blacklisted = '\', \''.join(blacklisted)
 
         query = f"""
-        with current_follo as (
-        select artist_id, 
-        array_agg(struct(followers, monthly_listeners )  order by last_updated desc limit 1)[safe_offset(0)] as follo_ml
-        from `rapsodie.rapsodie_main.artist_creatorabout`
-        group by artist_id
-        ) 
-        select 
-        array_agg(st.id order by st.album_release_date  DESC limit 1)[safe_offset(0)] as track,
-        from current_follo 
-        inner join `rapsodie.rapsodie_main.spotify_track_artist_map` as stam on stam.artist_id = current_follo.artist_id 
-        inner join (select track_id,
-        array_agg(struct(playcount, popularity )  order by last_updated desc limit 1)[safe_offset(0)] as ppop
-        from `rapsodie.rapsodie_main.spotify_track_playcount_trunc_latest` as stpltl
-        group by track_id)
-        as stptl on stptl.track_id = stam.track_id
-        inner join `rapsodie.rapsodie_main.spotify_track` as st on st.id = stam.track_id
-        where 
-        current_follo.follo_ml.followers < {max_followers}
-        and 
-        DATE_DIFF(CURRENT_DATE('Europe/Paris'), st.album_release_date, DAY) <= {max_timeframe}
+        with tracks as (
+        select isrc,
+        array_agg(struct(a.album_id, playcount, a.popularity, a.track_id, sa.type, sa.release_date) order by a.popularity DESC)[safe_offset(0)] as t_data,
+        from
+        (
+        select * , row_number() over(partition by track_id,album_id order by last_updated DESC) as rn
+        from `rapsodie.rapsodie_main.spotify_track_playcount_trunc_latest`
+        ) a
+        left join `rapsodie.rapsodie_main.spotify_track` as st on st.id = a.track_id
+        left join `rapsodie.rapsodie_main.spotify_album` as sa on sa.id = a.album_id
+        where sa.type not like 'compilation'
+        and rn=1 
+        and isrc is not null
+        group by isrc
+        )
+        select
+        tracks.isrc,
+        tracks.t_data.album_id as album_id,
+        tracks.t_data.playcount as playcount,
+        tracks.t_data.popularity as popularity,
+        tracks.t_data.track_id as track_id,
+        tracks.t_data.release_date as release_date,
+        stam.artist_id, 
+        is_primary, 
+        ac.followers, 
+        ac.monthly_listeners 
+        from tracks
+        left join `rapsodie.rapsodie_main.spotify_track_artist_map` as stam on stam.track_id = tracks.t_data.track_id
+        left join (select artist_id, followers, monthly_listeners
+        from
+        (
+        select * , row_number() over(partition by artist_id order by last_updated DESC) as rn
+        from `rapsodie.rapsodie_main.artist_creatorabout` 
+        ) where rn =1  ) as ac on ac.artist_id = stam.artist_id
+        where ac.artist_id is not null
         and
-        current_follo.artist_id not in ('{blacklisted}')
-
-        group by current_follo.artist_id, current_follo.follo_ml.monthly_listeners
-        order by current_follo.follo_ml.monthly_listeners DESC
+        is_primary = true
+        and followers < {max_followers}
+        and  DATE_DIFF(CURRENT_DATE(), t_data.release_date, DAY) <= {max_timeframe}
+        and  stam.artist_id not in ('{blacklisted}')
         """
 
         logger.info("Fetching data in database...")
-        bq_client = bigquery.Client()
-        data = bq_client.query(query).result()
-        rows = []
-        for r in data:
-            rows.append(r[0])
-
-
+        client = bigquery.Client()
+        df = client.query(query).to_dataframe()
+        df = df.sort_values(['release_date', 'playcount'],
+                            ascending=[False, False])
+        df = df.groupby(['artist_id']).first().reset_index()
+        df = df.sort_values(['monthly_listeners', 'followers'],
+                            ascending=[False, False])
         logger.info("Done!")
-        return rows[:50]
+        return list(df['track_id'].head(50))
 
