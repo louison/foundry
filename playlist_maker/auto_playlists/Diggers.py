56bcee03ab33102c2eb091ee8a4c523a8973182b
Louison Gitzinger <gitzingerlouison@gmail.com>
2020-12-01 15:30:38 +0100

    new diggers

--- playlist_maker/auto_playlists/Diggers.py
+++ playlist_maker/auto_playlists/Diggers.py
@@ -36,71 +36,53 @@ class Diggers(AutoPlaylist):
         oprimary_condition = " and is_primary = true" if only_primary else ""
 
         query = f"""
-       -- Select latest track statistics (playcount, popularity)
-        -- Remove tracks issued on compilation
-        -- group by isrc to avoid track duplicates
-        with tracks as (
-            select isrc,
-                   array_agg(struct(a.album_id, playcount, a.popularity, a.track_id, sa.type, sa.release_date)
-                             order by a.popularity DESC)[safe_offset(0)] as t_data,
-            from (
-                     select *, row_number() over (partition by track_id,album_id order by last_updated DESC) as rn
-                     from rapsodie.rapsodie_main.spotify_track_playcount_trunc_latest
-                 ) a
-                     left join rapsodie.rapsodie_main.spotify_track as st
-            on st.id = a.track_id
-                left join rapsodie.rapsodie_main.spotify_album as sa on sa.id = a.album_id
-            where sa.type not like 'compilation'
-              and rn=1
-              and isrc is not null
-            group by isrc
-        )
-        select tracks.isrc,
-               tracks.t_data.album_id     as album_id,
-               tracks.t_data.playcount    as playcount,
-               tracks.t_data.popularity   as popularity,
-               tracks.t_data.track_id     as track_id,
-               tracks.t_data.release_date as release_date,
-               stam.artist_id,
-               is_primary,
-               ac.followers,
-               ac.monthly_listeners
-        from tracks
-                 left join rapsodie.rapsodie_main.spotify_track_artist_map as stam
-        on stam.track_id = tracks.t_data.track_id
-            -- select latest statistics for artists (followers + monthly listeners)
-            left join (select artist_id, followers, monthly_listeners
-            from
-            (
-            select * , row_number() over (partition by artist_id order by last_updated DESC) as rn
-            from rapsodie.rapsodie_main.spotify_artist_creatorabout
-            ) where rn =1 ) as ac on ac.artist_id = stam.artist_id
-        where ac.artist_id is not null
-        {oprimary_condition}
-          and followers
-            < {max_followers}         -- select only tracks with an artist < X followers (currently)
-          and DATE_DIFF(CURRENT_DATE ()
-            , t_data.release_date
-            , DAY) <= {max_timeframe} -- select only tracks released withing max_timeframe days
-          and stam.artist_id not in ('{blacklisted}') -- select only tracks where the artist is not black listed
+with sap as ( 
+select 
+REPLACE(latest_release_uri, 'spotify:album:','') as album_id,
+artist_id, 
+latest_release_date, 
+sap.monthly_listeners,
+-- selectionner uniquement les dernières updates
+row_number() over (partition by artist_id order by last_updated DESC) as rn
+from `rapsodie.rapsodie_main.spotify_artist_page` as sap
+
+)
+select 
+array_agg(struct(track_id, stp.popularity, sap.latest_release_date) ORDER BY sap.latest_release_date desc)[offset(0)] as str,
+artist_id, 
+--array_agg(stp.popularity  ORDER BY stp.popularity desc)[offset(0)] as pop
+from sap
+left join (
+  with stp as (
+    select
+      playcount,
+      popularity, 
+      album_id, 
+      track_id,
+      row_number() over (partition by track_id order by last_updated DESC) as rn
+    from
+      `rapsodie.rapsodie_main.spotify_track_playcount`
+  )
+    select * from stp
+    where rn = 1
+)
+as stp on sap.album_id = stp.album_id
+where sap.rn = 1
+and stp.track_id is not null
+and  DATE_DIFF(CURRENT_DATE ()
+            , sap.latest_release_date 
+            , DAY) <= 30
+and sap.monthly_listeners <= 10000
+group by sap.artist_id
+order by str.popularity desc;
         """
 
         logger.info("Fetching data in database...")
         client = bigquery.Client()
-        df = client.query(query).to_dataframe()
-        # Sort tracks by most recent release and the by playcount
-        df = df.sort_values(
-            ["popularity", "playcount", "release_date"],
-            ascending=[False, False, False]
-        )
-        # Group all tracks by artist_id an keep the first one (the most recent one and the most streamed)
-        df = df.groupby(["artist_id"]).first().reset_index()
-        # Order results by monthely_listeners and number of followers
-        df = df.sort_values(
-            ["monthly_listeners", "followers"], ascending=[False, False]
-        )
-        logger.info("Done!")
-        tracks = list(df["track_id"].head(50))
+        rows = client.query(query).result()
+        tracks = []
+        for row in rows:
+            tracks.append(row['str']['track_id'])
         self.announcement_data = tracks
         return tracks
 
