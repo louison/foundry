7575e299d9c2ae5cbc08c49382a99c9a503cea12
Louison Gitzinger <gitzingerlouison@gmail.com>
2021-02-01 10:09:05 +0100

    release new query for Diggers

--- playlist_maker/auto_playlists/Diggers.py
+++ playlist_maker/auto_playlists/Diggers.py
@@ -36,46 +36,84 @@ class Diggers(AutoPlaylist):
         oprimary_condition = " and is_primary = true" if only_primary else ""
 
         query = f"""
-with sap as ( 
-select 
-REPLACE(latest_release_uri, 'spotify:album:','') as album_id,
-artist_id, 
-latest_release_date, 
-sap.monthly_listeners,
--- selectionner uniquement les dernières updates
-row_number() over (partition by artist_id order by last_updated DESC) as rn
-from `rapsodie.rapsodie_main.spotify_artist_page` as sap
-
-)
-select 
-array_agg(struct(track_id, stp.popularity, sap.latest_release_date) ORDER BY sap.latest_release_date desc)[offset(0)] as str,
-artist_id, 
---array_agg(stp.popularity  ORDER BY stp.popularity desc)[offset(0)] as pop
-from sap
-left join (
-  with stp as (
-    select
-      playcount,
-      popularity, 
-      album_id, 
+WITH
+  small_artists AS (
+  SELECT
+    DISTINCT artist_id,
+    sap.name,
+    sac.monthly_listeners,
+    sap.popularity
+  FROM
+    `rapsodie.rapsodie_main.spotify_artist_creatorabout` AS sac
+  INNER JOIN
+    `rapsodie.rapsodie_main.spotify_artist_popularity` AS sap
+  ON
+    sap.id = artist_id
+  WHERE
+    sac.monthly_listeners <= 15000
+    AND CAST(sac.last_updated AS date) >= CURRENT_DATE()
+    AND CAST(sap.last_updated AS date) >= CURRENT_DATE())
+SELECT
+  *
+FROM (
+  SELECT
+    DISTINCT small_artists.artist_id,
+    monthly_listeners,
+    small_artists.name artist_name,
+    st.id track_id,
+    st.name track_name,
+    small_artists.popularity,
+    stp.playcount AS playcount,
+    stp.last_updated,
+    ROW_NUMBER() OVER (PARTITION BY small_artists.artist_id ORDER BY st.album_release_date DESC) AS row_rank,
+    st.album_release_date,
+    sal.album_type,
+    (
+      -- Get first release date for an artist
+    SELECT
+      MIN(st.album_release_date)
+    FROM
+      `rapsodie.rapsodie_main.spotify_track` AS st
+    CROSS JOIN
+      `rapsodie.rapsodie_main.spotify_artist` AS sa
+    WHERE
+      sa.id IN UNNEST(st.artists)
+      AND sa.id = small_artists.artist_id
+    GROUP BY
+      sa.id) AS first_release_date
+  FROM
+    small_artists
+  CROSS JOIN
+    `rapsodie.rapsodie_main.spotify_track` AS st
+  LEFT JOIN
+    `rapsodie.rapsodie_main.spotify_album` AS sal
+  ON
+    st.album_id = sal.id
+  LEFT JOIN (
+    SELECT
       track_id,
-      row_number() over (partition by track_id order by last_updated DESC) as rn
-    from
-      `rapsodie.rapsodie_main.spotify_track_playcount`
-  )
-    select * from stp
-    where rn = 1
-)
-as stp on sap.album_id = stp.album_id
-where sap.rn = 1
-and stp.track_id is not null
-and  DATE_DIFF(CURRENT_DATE ()
-            , sap.latest_release_date 
-            , DAY) <= 30
-and sap.monthly_listeners <= 10000
-and sap.artist_id not in ('{blacklisted}') 
-group by sap.artist_id
-order by str.popularity desc;
+      playcount,
+      last_updated,
+      ROW_NUMBER() OVER (PARTITION BY stp.track_id ORDER BY last_updated DESC) AS row_rank,
+    FROM
+      `rapsodie.rapsodie_main.spotify_track_playcount` AS stp ) AS stp
+  ON
+    st.id = stp.track_id
+  WHERE
+    small_artists.artist_id IN UNNEST(st.artists)
+    AND small_artists.artist_id not in ('{blacklisted}')
+    AND st.album_release_date > CURRENT_DATE() - 60
+    AND sal.album_type != 'compilation'
+    AND '0LyfQWJT6nXafLPZqxe9Of' not in unnest(sal.artists)
+    -- Remove featurings ?
+    --AND ARRAY_LENGTH(st.artists) < 2
+    )
+WHERE
+  row_rank = 1
+  AND first_release_date > CURRENT_DATE() - 1095
+ORDER BY
+  popularity DESC,
+  playcount DESC
         """
 
         logger.info("Fetching data in database...")
@@ -83,7 +121,7 @@ order by str.popularity desc;
         rows = client.query(query).result()
         tracks = []
         for row in rows:
-            tracks.append(row['str']['track_id'])
+            tracks.append(row['track_id'])
         self.announcement_data = tracks
         return tracks
 
