b5de12fd8d81d6ed9d2dd73f08ffc68dca422302
Louison Gitzinger <gitzingerlouison@gmail.com>
2021-02-02 10:15:35 +0100

    playlist_id is now prevalent over playlist_name

--- playlist_maker/entrypoint.py
+++ playlist_maker/entrypoint.py
@@ -83,55 +83,55 @@ def generic(message=None):
     user.connect()
     user.fetch()
     playlist_name = message["playlist_name"]
-    if playlist_name:
-        playlists = user.get_playlists()
+    playlist_id = message.get('playlist_id', None)
+    playlists = user.get_playlists()
+    playlist_object = None
+    if playlist_id:
+        playlist_object = client.playlist(message.get("playlist_id"))
+
+    if not playlist_object:
         names = list(map(lambda x: x["name"], playlists))
         if playlist_name in names:
-            push_method = message.get("push_method")
-            if push_method not in PUSH_METHODS:
-                raise ValueError(f"push method: {push_method} not supported")
-            if message.get("override", False):
-                playlist_object = list(
-                    filter(lambda x: x["name"] == playlist_name, playlists)
-                )[0]
-                if push_method == "append":
-                    tracks = user.get_playlist_tracks(playlist_object["id"])
-                    tracks_ids = list(map(lambda x: x["track"]["id"], tracks))
-                    message["tracks"].extend(tracks_ids)
-                elif push_method == "keep":
-                    tracks = user.get_playlist_tracks(playlist_object["id"])
-                    tracks_ids = list(map(lambda x: x["track"]["id"], tracks))
-                    message["tracks"] = tracks_ids
-            else:
-                raise ValueError(
-                    f"Playlist with name: {playlist_name}"
-                    f" already exist for user {message['username']} use -o to override"
-                )
-        else:
-            playlist_object = client.user_playlist_create(
-                user.username,
-                playlist_name,
-                public=message.get("public", False),
-                description=message.get("description", ""),
-            )
-    else:
-        playlist_object = client.playlist(message["playlist_id"])
+            print(playlist_name)
+            playlist_object = list(
+                filter(lambda x: x["name"] == playlist_name, playlists)
+            )[0]
+            print(playlist_object)
+    if playlist_object and not message['override']:
+        raise ValueError(
+            f"Playlist with name: {playlist_name} and id: {playlist_id}"
+            f" already exist for user {message['username']} use -o to override"
+        )
+    if not playlist_object:
+        playlist_object = client.user_playlist_create(
+            user.username,
+            playlist_name,
+            public=message.get("public", False),
+            description=message.get("description", ""),
+        )
+
+    push_method = message.get("push_method")
+    if push_method == "append":
+        tracks = user.get_playlist_tracks(playlist_object["id"])
+        tracks_ids = list(map(lambda x: x["track"]["id"], tracks))
+        message["tracks"].extend(tracks_ids)
+    elif push_method == "keep":
+        tracks = user.get_playlist_tracks(playlist_object["id"])
+        tracks_ids = list(map(lambda x: x["track"]["id"], tracks))
+        message["tracks"] = tracks_ids
 
     track_chunks = chunks(message["tracks"], 100)
     # First empty the playlist
-    client.user_playlist_replace_tracks(user.username, playlist_object["id"],
-                                        tracks=[])
+    client.playlist_replace_items(playlist_object["id"], [])
     # Then append new tracks by batch of 100
     for chunk in track_chunks:
-        client.user_playlist_add_tracks(
-            user.username, playlist_object["id"], tracks=chunk
-        )
+        client.playlist_add_items(playlist_object["id"], chunk)
 
-    client.user_playlist_change_details(
-        message["username"],
+    client.playlist_change_details(
         playlist_object["id"],
-        description=message.get("description", ""),
-        public=message.get("public", False),
+        name=message.get('playlist_name', playlist_object.get('name', "")),
+        description=message.get("description", playlist_object.get('description', "")),
+        public=message.get("public",  playlist_object.get('public', False)),
     )
     if message.get('announcements'):
         announce(message.get('announcements'))
